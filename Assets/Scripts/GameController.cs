999e71a529cae765e9d86fa94718719f31b16843
Unknown <srjurquhart@icloud.com>
2018-01-23 17:24:23 -0800

    Version 6! Game Over
    *CHANGE LOG*
    - Added a Game Over screen (press "E" to use)
    - Changed the behavior of Game Manager
    - Fixes to Start Menu
    
    **KNOWN BUGS**
    - The Crate falls through the floor

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -1,10 +1,57 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class GameController : MonoBehaviour {
 
-	public void LoadGame()
+    public static GameController instance = null;
+    
+    private void Awake()
     {
-        SceneManager.LoadScene("Suenos_V04", LoadSceneMode.Single);
-    }    
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else if (instance != this)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            GameOver();
+        }
+    }
+
+    public static void LoadGame()
+    {
+        SceneManager.LoadScene(1, LoadSceneMode.Single);
+    }
+
+    public void LoadGameWrapper()
+    {
+        LoadGame();
+    }
+    
+    public static void MainMenu()
+    {
+        SceneManager.LoadScene("Suenos_MainMenu", LoadSceneMode.Single);
+    }
+
+    public void MainMenuWrapper()
+    {
+        MainMenu();
+    }
+
+    public static void GameOver()
+    {
+        SceneManager.LoadScene("Suenos_GameOver", LoadSceneMode.Additive);
+        
+    }
+
 }
---
+++ Assets/Scripts/GameOver.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameOver : MonoBehaviour
+{
+
+    public Text go_text;
+    public Image go_background;
+
+    public Text message;
+
+    private bool canCont = false;
+
+    private void Start()
+    {
+        StartCoroutine(FadeGAGraphic());
+        StartCoroutine(FadeGAText());
+        StartCoroutine(SlowTime());
+        StartCoroutine(Restart());
+    }
+
+    private void Update()
+    {
+        if (Input.anyKeyDown && canCont)
+        {
+            Time.timeScale = 1;
+            GameController.MainMenu();
+        }
+    }
+
+    private IEnumerator FadeGAText()
+    {
+        for (float i = 0; i <= 1f; i += 0.01f)
+        {
+            Color c_text = go_text.material.color;
+            c_text.a = i;
+            go_text.material.color = c_text;
+
+            yield return null;
+        }
+    }
+
+    private IEnumerator FadeGAGraphic()
+    {
+        for (float i = 0; i <= 1600; i += 10)
+        {
+            go_background.rectTransform.SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, i);
+            go_background.rectTransform.SetSizeWithCurrentAnchors(RectTransform.Axis.Vertical, i / 2);
+
+            yield return null;
+        }
+    }
+
+    private IEnumerator SlowTime()
+    {
+        for (float i = 1; i >= 0; i -= 0.01f)
+        {
+            Time.timeScale = i;
+
+            yield return null;
+        }
+    }
+
+    private IEnumerator Restart()
+    {
+        yield return new WaitForSecondsRealtime(3);
+        message.text = "Press Any Key to Continue";
+        canCont = true;
+    }
+}
+
